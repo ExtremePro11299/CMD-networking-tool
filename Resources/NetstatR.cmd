1ee5d3288b168a443c075a59799751cc1f344109
ExtremePro11299 <134209046+ExtremePro11299@users.noreply.github.com>
2023-08-31 09:51:27 +0200

    Fix result saving not working

--- Resources/NetstatR.cmd
+++ Resources/NetstatR.cmd
@@ -6,12 +6,13 @@ echo Would you like to save the results to a file?
 echo 1) No
 echo 2) Yes
 set /p savetype=
-if %savetype%==1 goto tsoff
-
+if %savetype%==1 call :tsoff
+if %savetype%==1 goto tson
+goto endChoice
 :tsoff
 netstat -r
 echo Netstat completed.
-goto endChoice
+goto :eof
 :tson
 cd..
 netstat -r >> SavedResults\NetstatRResult-%RANDOM%.txt
@@ -21,6 +22,6 @@ echo What do you want to do? (type the number)
 echo 1) Netstat again
 echo 2) Exit
 set /p input=
-if %input%==1 goto testprep
+if %input%==1 goto start
 if %input%==2 exit
-if %input%==3 goto endChoice
+goto endChoice
